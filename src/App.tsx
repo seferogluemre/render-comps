f173623c0ba5a136fba656d329b5df3b2ab1bacf
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-19 00:46:09 +0300

    lifecycle test comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function LifeCycleTestComponent() {
+  const [counter, setCounter] = useState(0);
+
   useEffect(() => {
     // 1.Aşama comp render edildi (Mount olması)
     console.log("Şuan render edildi");
   }, []);
 
-  return <div>Component İçerigi</div>;
+  useEffect(() => {
+    // 2.Aşama :güncellenmesi (rerender oldugu an)
+    console.log("Component güncellendi yani (rerender oldu)");
+  });
+
+  useEffect(() => {
+    // Componentin renderdan çıkış anı unmount olması
+    return () => {
+      console.log("Component Renderdan çıktı.");
+    };
+  }, []);
+
+  return (
+    <div>
+      <div>sayaç {counter}</div>
+      <button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>
+        Sayacı Arttır
+      </button>
+    </div>
+  );
 }
 
 function App() {
+  const [showLifeCycleTestComp, setShowLifeCycleTestComp] =
+    useState<boolean>(true);
+
   return (
     <>
-      <LifeCycleTestComponent />
+      <div>
+        <div>{showLifeCycleTestComp && <LifeCycleTestComponent />}</div>
+        <button
+          onClick={() => setShowLifeCycleTestComp((prevTest) => !prevTest)}
+        >
+          Toggle Life cycle test component
+        </button>
+      </div>
     </>
   );
 }
